7e5eb7ebaaf212de9fdfa235cd5c247092602c2b
diegoriya09 <juandiegoriveray920@gmail.com>
2025-06-19 15:53:22 -0500

    v25

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stetson LATAM</title>
     <meta name="description" content="Stetson LATAM - Legendary Hats for Latin America and Central America">
-    <link rel="stylesheet" href="css/index.css?v=24">
+    <link rel="stylesheet" href="css/index.css?v=25">
     <link rel="stylesheet" href="css/carrousel.css?v=25">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
