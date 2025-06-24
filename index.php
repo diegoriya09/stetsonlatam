fec90dcd3e5329b53993eede7943da24ce725076
diegoriya09 <juandiegoriveray920@gmail.com>
2025-06-24 10:13:15 -0500

    logo

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         <!-- Puedes agregar el logo aquí si deseas -->
         <div class="nav-left">
             <a href="index.php">
-                <h2 class="logo">STETSON LATAM<sup>&reg;</sup></h2>
+                <h2 class="logo">STETSON LATAM<span class="reg">&reg;</sup></h2>
             </a>
         </div>
         <!-- Enlaces centrados -->
