40cff829ff1eac0ef0e84b04e18fd268c5f286d8
diegoriya09 <juandiegoriveray920@gmail.com>
2025-06-24 16:16:18 -0500

    diferencia entre feature hats y hats

--- productos.php
+++ productos.php
@@ -7,7 +7,7 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM productos";
+$sql = "SELECT * FROM productos WHERE category = 'hats' AND is_featured = 1";
 $resultado = $conn->query($sql);
 
 if ($resultado && $resultado->num_rows > 0) {
