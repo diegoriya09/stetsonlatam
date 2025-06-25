a03a8306bb094525bf3249b54a78cf527b196576
diegoriya09 <juandiegoriveray920@gmail.com>
2025-06-25 11:19:56 -0500

    ordenamiento

---
+++ footer.php
@@ -0,0 +1,53 @@
+<footer class="site-footer">
+        <div class="footer-columns">
+            <div class="footer-column">
+            <h4>CUSTOMER SERVICE</h4>
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Shipping</a></li>
+                <li><a href="#">Returns & Exchanges</a></li>
+                <li><a href="#">Order Status</a></li>
+                <li><a href="#">Fit Guide</a></li>
+                <li><a href="#">Gift Cards</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+            </div>
+            <div class="footer-column">
+            <h4>OUR COMPANY</h4>
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Philanthropy</a></li>
+                <li><a href="#">Wholesale</a></li>
+            </ul>
+            </div>
+            <div class="footer-column">
+            <h4>STORES</h4>
+            <ul>
+                <li><a href="#">Store Locator</a></li>
+                <li><a href="#">Stetson Japan</a></li>
+                <li><a href="#">Stetson Europe</a></li>
+                <li><a href="#">Stetson Australia</a></li>
+            </ul>
+            </div>
+            <div class="footer-column">
+            <h4>CONNECT</h4>
+            <ul>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">YouTube</a></li>
+                <li><a href="#">Pinterest</a></li>
+            </ul>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; 2025 Stetson Latam. All Rights Reserved.</p>
+            <div class="footer-links">
+            <a href="#">Terms & Conditions</a> |
+            <a href="#">Privacy Policy</a> |
+            <a href="#">California Privacy Notice</a> |
+            <a href="#">Accessibility Mode</a> |
+            <a href="#">Do Not Sell Or Share My Personal Information</a>
+            </div>
+        </div>
+    
