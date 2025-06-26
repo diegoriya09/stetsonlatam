5395335f2ad545d95a46d5b4e8c05eb8afb3ac6c
diegoriya09 <juandiegoriveray920@gmail.com>
2025-06-26 10:55:31 -0500

    bug carrito

--- cart.php
+++ cart.php
@@ -3,10 +3,10 @@
             <h3>My cart</h3>
             <span class="close-sidebar" id="cerrar-carrito">&times;</span>
         </div>
+        <p id="carrito-vacio" style="display: none; text-align: center; margin-top: 2rem; color: #555;">
+            Your cart is currently empty!
+        </p>
         <div class="sidebar-content" id="carrito-items">
-            <p id="carrito-vacio" style="display: none; text-align: center; margin-top: 2rem; color: #555;">
-                Your cart is currently empty!
-            </p>
             <!-- Aquí se mostrarán los productos del carrito dinámicamente -->
         </div>
         <div class="sidebar-footer">
