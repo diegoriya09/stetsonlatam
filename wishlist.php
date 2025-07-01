1ed7f3bdd2ab69fe3060d9c20f8253fa83e9e445
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-01 10:44:39 -0500

    index.css en wish

--- wishlist.php
+++ wishlist.php
@@ -25,6 +25,7 @@ $conn->close();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Wishlist - Stetson Latam</title>
+  <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="css/wishlist.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
