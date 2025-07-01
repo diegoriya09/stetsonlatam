99fa6772cc29186c99900ae98b03949047eeb7a1
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-01 09:11:02 -0500

    diseño

--- producto.php
+++ producto.php
@@ -34,6 +34,7 @@ $conn->close();
   <link rel="icon" href="img/logo.png" type="image/x-icon">
   <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="css/hats.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="css/product.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap" rel="stylesheet">
 </head>
