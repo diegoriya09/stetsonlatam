688a49ae772812cd91730d2125aa0ce532514ee5
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-04 09:40:37 -0500

    optimización

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stetson Latam</title>
     <meta name="description" content="Stetson LATAM - Legendary Hats for Latin America and Central America">
-    <link rel="icon" href="img/logo.png" type="image/x-icon">
+    <link rel="icon" href="img/logo.webp" type="image/x-icon">
     <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap" rel="stylesheet">
--- navbar.php
+++ navbar.php
@@ -24,12 +24,12 @@
             <div class="hamburger" id="hamburger">&#9776;</div>
             <!-- Solo para escritorio -->
             <div class="desktop-icons">
-                <a href="#" id="open-user-modal"><img src="/img/user.png" alt="User" class="icon" /></a>
-                <a id="logout-btn" style="display: none;"><img src="/img/logout.png" alt="Logout" class="icon"></a>
-                <a href="#"><img src="/img/search.png" alt="Search" class="icon" /></a>
-                <a href="wishlist.php"><img src="/img/heart.png" alt="Wishlist" class="icon" /></a>
+                <a href="#" id="open-user-modal"><img src="/img/user.webp" alt="User" class="icon" /></a>
+                <a id="logout-btn" style="display: none;"><img src="/img/logout.webp" alt="Logout" class="icon"></a>
+                <a href="#"><img src="/img/search.webp" alt="Search" class="icon" /></a>
+                <a href="wishlist.php"><img src="/img/heart.webp" alt="Wishlist" class="icon" /></a>
                 <div class="cart-wrapper">
-                    <a id="btn-carrito"><img src="/img/cart.png" alt="Cart" class="icon" /></a>
+                    <a id="btn-carrito"><img src="/img/cart.webp" alt="Cart" class="icon" /></a>
                 </div>
             </div>
         </div>
