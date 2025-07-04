b9450e6ffdb5aaeffd81a45e4cbebb3bcb03b92a
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-04 09:37:20 -0500

    optimización

--- stories.php
+++ stories.php
@@ -2,13 +2,13 @@
         <h2>Our Legacy</h2>
         <div class="card-grid">
             <article class="card-item">
-                <img src="img/story1.png" alt="Origen Stetson">
+                <img src="img/story1.webp" alt="Origen Stetson">
                 <h3>Cowboy Heritage</h3>
                 <p>From the American West to Latin America, discover the history behind the legendary hat.
                 </p>
             </article>
             <article class="card-item">
-                <img src="img/story2.png" alt="Fabricación artesanal">
+                <img src="img/story2.webp" alt="Fabricación artesanal">
                 <h3>Centennial Crafts</h3>
                 <p>Handmade hats with techniques that have lasted for generations.</p>
             </article>
