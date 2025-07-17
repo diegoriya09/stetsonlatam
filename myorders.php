bc156d12808a95a553126044afc9cb105f7708fe
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-17 10:51:51 -0500

    prueba

--- myorders.php
+++ myorders.php
@@ -24,6 +24,7 @@ $pedidos = $result->fetch_all(MYSQLI_ASSOC);
    <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>">
    <link rel="stylesheet" href="css/product.css?v=<?php echo time(); ?>">
    <link rel="stylesheet" href="css/myorders.css?v=<?php echo time(); ?>">
+   <link rel="stylesheet" href="css/wishlist.css?v=<?php echo time(); ?>">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap" rel="stylesheet">
 </head>
