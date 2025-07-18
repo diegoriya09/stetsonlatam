fb390e0cc2b4d3a54d38ac64f48939c5fe079418
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-18 09:42:40 -0500

    error

--- php/cart/checkout.php
+++ php/cart/checkout.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'php/conexion.php';
+require_once 'conexion.php';
 require_once '../../vendor/autoload.php';
 
 if (session_status() !== PHP_SESSION_ACTIVE) session_start();
