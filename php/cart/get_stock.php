c5d7815e2831935802c1931c82955a051cd53514
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-21 11:43:26 -0500

    depuración!

--- php/cart/get_stock.php
+++ php/cart/get_stock.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'php/conexion.php';
 
 $product_id = isset($_GET['product_id']) ? intval($_GET['product_id']) : 0;
