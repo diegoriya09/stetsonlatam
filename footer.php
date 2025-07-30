75a3b9ec94826f48cf55464e91449b5c4a308833
diegoriya09 <juandiegoriveray920@gmail.com>
2025-07-30 10:05:35 -0500

    footer update

--- footer.php
+++ footer.php
@@ -7,9 +7,8 @@
                     <li><a href="contacto.php">Contact Us</a></li>
                     <li><a href="#">Shipping</a></li>
                     <li><a href="#">Returns & Exchanges</a></li>
-                    <li><a href="#">Order Status</a></li>
+                    <li><a href="#" id="open-user-modal">Order Status</a></li>
                     <li><a href="#">Fit Guide</a></li>
-                    <li><a href="#">Gift Cards</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
             </div>
@@ -25,18 +24,16 @@
                 <h4>STORES</h4>
                 <ul>
                     <li><a href="#">Store Locator</a></li>
-                    <li><a href="#">Stetson Japan</a></li>
-                    <li><a href="#">Stetson Europe</a></li>
-                    <li><a href="#">Stetson Australia</a></li>
+                    <li><a href="https://stetson.jp/">Stetson Japan</a></li>
+                    <li><a href="https://stetson.eu/">Stetson Europe</a></li>
+                    <li><a href="https://stetsonaustralia.com.au/">Stetson Australia</a></li>
                 </ul>
             </div>
             <div class="footer-column">
                 <h4>CONNECT</h4>
                 <ul>
-                    <li><a href="#">Instagram</a></li>
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">YouTube</a></li>
-                    <li><a href="#">Pinterest</a></li>
+                    <li><a href="https://www.instagram.com/dinalsom?utm_source=ig_web_button_share_sheet&igsh=OTZkY3d3bzdnNHFh">Instagram</a></li>
+                    <li><a href="https://www.facebook.com/distribuidora.nacionaldesombreros?locale=es_LA">Facebook</a></li>
                 </ul>
             </div>
         </div>
