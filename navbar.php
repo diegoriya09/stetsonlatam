2fa51b8cce75f14fe4761baa6431160f17079e91
diegoriya09 <juandiegoriveray920@gmail.com>
2025-08-11 11:02:29 -0500

    cambios navbar

--- navbar.php
+++ navbar.php
@@ -1,40 +1,58 @@
-<nav class="main-nav">
-    <div class="nav-left">
-        <a href="index.php">
-            <h2 class="logo">STETSON LATAM<span class="reg">&reg;</sup></h2>
-        </a>
-    </div>
-    <div class="mobile-menu" id="mobile-menu">
-        <div class="mobile-search">
-            <input type="text" placeholder="Buscar..." />
-        </div>
-        <ul>
-            <li><a href="hats.php">Hats</a></li>
-            <li><a href="caps.php">Caps</a></li>
-            <li><a href="index.php#stories">Stories</a></li>
-            <li><a href="myorders.php" id="mis-pedidos-link-mobile" style="display:none;">My orders</a></li>
-        </ul>
-    </div>
-    <!-- Enlaces centrados -->
-    <ul class="nav-center">
-        <li><a href="hats.php">Hats</a></li>
-        <li><a href="caps.php">Caps</a></li>
-        <li><a href="index.php#stories">Stories</a></li>
-        <li><a href="myorders.php" id="mis-pedidos-link" style="display: none;">My orders</a></li>
-    </ul>
-    <!-- Íconos a la derecha -->
-    <div class="nav-right">
-        <!-- Hamburguesa visible solo en móvil -->
-        <div class="hamburger" id="hamburger">&#9776;</div>
-        <!-- Solo para escritorio -->
-        <div class="desktop-icons">
-            <a href="#" id="open-user-modal"><img src="/img/user.webp" alt="User" class="icon" loading="lazy" /></a>
-            <a id="logout-btn" style="display: none;"><img src="/img/logout.webp" alt="Logout" class="icon" loading="lazy" /></a>
-            <a href="#"><img src="/img/search.webp" alt="Search" class="icon" loading="lazy" /></a>
-            <a href="wishlist.php"><img src="/img/heart.webp" alt="Wishlist" class="icon" loading="lazy" /></a>
-            <div class="cart-wrapper">
-                <a id="btn-carrito"><img src="/img/cart.webp" alt="Cart" class="icon" loading="lazy" /></a>
+<header class="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f4f2f0] px-10 py-3">
+    <div class="flex items-center gap-8">
+        <div class="flex items-center gap-4 text-[#181411]">
+            <div class="size-4">
+                <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z" fill="currentColor"></path>
+                </svg>
             </div>
+            <h2 class="text-[#181411] text-lg font-bold leading-tight tracking-[-0.015em]">Stetson Latam</h2>
+        </div>
+        <div class="flex items-center gap-9">
+            <a class="text-[#181411] text-sm font-medium leading-normal" href="index.php">New</a>
+            <a class="text-[#181411] text-sm font-medium leading-normal" href="hats.php">Hats</a>
+            <a class="text-[#181411] text-sm font-medium leading-normal" href="caps.php">Caps</a>
         </div>
     </div>
-</nav>
+    <div class="flex flex-1 justify-end gap-8">
+        <label class="flex flex-col min-w-40 !h-10 max-w-64">
+            <div class="flex w-full flex-1 items-stretch rounded-lg h-full">
+                <div
+                    class="text-[#887563] flex border-none bg-[#f4f2f0] items-center justify-center pl-4 rounded-l-lg border-r-0"
+                    data-icon="MagnifyingGlass"
+                    data-size="24px"
+                    data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+                        <path
+                            d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+                    </svg>
+                </div>
+                <input
+                    placeholder="Search"
+                    class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#181411] focus:outline-0 focus:ring-0 border-none bg-[#f4f2f0] focus:border-none h-full placeholder:text-[#887563] px-4 rounded-l-none border-l-0 pl-2 text-base font-normal leading-normal"
+                    value="" />
+            </div>
+        </label>
+        <div class="flex gap-2">
+            <button
+                class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 bg-[#f4f2f0] text-[#181411] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5">
+                <div class="text-[#181411]" data-icon="Heart" data-size="20px" data-weight="regular"><a href="wishlist.php"></a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                        <path
+                            d="M178,32c-20.65,0-38.73,8.88-50,23.89C116.73,40.88,98.65,32,78,32A62.07,62.07,0,0,0,16,94c0,70,103.79,126.66,108.21,129a8,8,0,0,0,7.58,0C136.21,220.66,240,164,240,94A62.07,62.07,0,0,0,178,32ZM128,206.8C109.74,196.16,32,147.69,32,94A46.06,46.06,0,0,1,78,48c19.45,0,35.78,10.36,42.6,27a8,8,0,0,0,14.8,0c6.82-16.67,23.15-27,42.6-27a46.06,46.06,0,0,1,46,46C224,147.61,146.24,196.15,128,206.8Z"></path>
+                    </svg>
+                </div>
+            </button>
+            <button
+                class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 bg-[#f4f2f0] text-[#181411] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5">
+                <div class="text-[#181411]" data-icon="ShoppingBag" data-size="20px" data-weight="regular">
+                    <a id="btn-carrito"></a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                        <path
+                            d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,160H40V56H216V200ZM176,88a48,48,0,0,1-96,0,8,8,0,0,1,16,0,32,32,0,0,0,64,0,8,8,0,0,1,16,0Z"></path>
+                    </svg>
+                </div>
+            </button>
+        </div>
+    </div>
+</header>
