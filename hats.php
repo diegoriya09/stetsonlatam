72c87888cafbf77375730b7339140073d79bc9c6
diegoriya09 <juandiegoriveray920@gmail.com>
2025-08-11 12:57:14 -0500

    negro

--- hats.php
+++ hats.php
@@ -100,7 +100,7 @@ $conn->close();
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z" fill="currentColor"></path>
               </svg>
             </div>
-            <h2 class="text-[#151514] text-lg font-bold leading-tight tracking-[-0.015em]"><a href="index.php">Stetson Latam</a></h2>
+            <h2 class="text-[#181411] text-lg font-bold leading-tight tracking-[-0.015em]"><a href="index.php">Stetson Latam</a></h2>
           </div>
           <div class="flex items-center gap-9">
             <a class="text-[#151514] text-sm font-medium leading-normal" href="hats.php">Hats</a>
