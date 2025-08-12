6b4d72a94e7697a6634a0f69c1a2b41ba52bc560
diegoriya09 <juandiegoriveray920@gmail.com>
2025-08-12 11:59:52 -0500

    profile

--- profile.php
+++ profile.php
@@ -399,6 +399,7 @@
       </footer>
     </div>
   </div>
+  <?php include 'modal.php'; ?>
   <script src="js/auth.js?v=<?php echo time(); ?>"></script>
   <script src="js/index.js?v=<?php echo time(); ?>"></script>
 </body>
