1620d736d55664e384d4c1f0404bd206d184bc3f
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-04 11:21:16 -0500

    prueba

--- header.php
+++ header.php
@@ -55,7 +55,7 @@ if (!empty($categorias_flat)) {
 <header
   class="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f3f2f2] px-10 py-5 h-20">
   <div class="flex-1 flex justify-start">
-    <a href="index.php" aria-label="Stetson Latam">
+    <a href="https://stetsonlatam.com/" aria-label="Stetson Latam">
       <img src="img/logo.svg" alt="Logo de Stetson Latam" class="h-5 w-auto">
     </a>
   </div>
