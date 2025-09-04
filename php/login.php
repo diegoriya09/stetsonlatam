deffd9853f1d3a460524f701fbe58fcbd1f224a9
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-04 16:53:18 -0500

    error2

--- php/login.php
+++ php/login.php
@@ -9,8 +9,8 @@ session_start();
 error_log("POST token: " . ($_POST['csrf_token'] ?? ''));
 error_log("SESSION token: " . ($_SESSION['csrf_token'] ?? ''));
 
-require 'conexion';
-require '../vendor/autoload';
+require 'conexion.php';
+require '../vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
