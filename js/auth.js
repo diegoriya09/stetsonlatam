7a4a1279bef68d208bf85559d65737146c4229aa
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-04 16:51:32 -0500

    error

--- js/auth.js
+++ js/auth.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const formData = new FormData(loginForm);
 
             try {
-                const response = await fetch('../php/login.php', {
+                const response = await fetch('../php/login', {
                     method: 'POST',
                     body: formData,
                     credentials: 'same-origin'
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const formData = new FormData(registerForm);
 
             try {
-                const response = await fetch('php/register.php', {
+                const response = await fetch('php/register', {
                     method: 'POST',
                     body: formData,
                     credentials: 'same-origin'
