adc737c2fbf90ab59ef35238b5b8e66bbd3b028c
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-05 09:51:29 -0500

    solucion definitiva

--- js/cart.js
+++ js/cart.js
@@ -20,7 +20,7 @@ function addToCart(productData) {
   }
 
   // RUTA CORREGIDA
-  fetch('(/php/cart/add_to_cart.php', {
+  fetch('/php/cart/add_to_cart', {
     method: 'POST',
     headers: {
       'Authorization': 'Bearer ' + jwt,
@@ -147,7 +147,7 @@ document.getElementById('cart-items-container')?.addEventListener('click', async
       return;
     }
 
-    await postToCartAPI('/php/cart/update_cart.php', { cart_item_id: cart_item_id, cantidad: newQty });
+    await postToCartAPI('/php/cart/update_cart', { cart_item_id: cart_item_id, cantidad: newQty });
     loadCart(); // Recarga el carrito para mostrar el cambio
   }
 
@@ -166,7 +166,7 @@ document.getElementById('cart-items-container')?.addEventListener('click', async
       confirmButtonText: 'Yes, remove it!'
     }).then(async (result) => {
       if (result.isConfirmed) {
-        await postToCartAPI('/php/cart/remove_from_cart.php', { cart_item_id: cart_item_id });
+        await postToCartAPI('/php/cart/remove_from_cart', { cart_item_id: cart_item_id });
         loadCart(); // Recarga el carrito
       }
     });
