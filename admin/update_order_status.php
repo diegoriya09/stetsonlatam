842fa03cf39cb03aa79927e25fe619588767d577
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-05 14:46:38 -0500

    error solucionado

--- admin/update_order_status.php
+++ admin/update_order_status.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->bind_param("si", $estado, $order_id);
 
     if ($stmt->execute()) {
-        header("Location: admin.php?msg=Estado actualizado");
+        header("Location: admin?msg=Estado actualizado");
     } else {
         echo "Error al actualizar el estado.";
     }
