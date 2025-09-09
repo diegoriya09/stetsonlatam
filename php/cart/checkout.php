69198c7b89928367eeb958a4acc7767fc81b5e3a
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-09 17:02:47 -0500

    cambio

--- php/cart/checkout.php
+++ php/cart/checkout.php
@@ -121,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $telefono = $_POST['telefono'] ?? '';
         $metodo = $_POST['metodo'] ?? '';
 
-        $stmt_order = $conn->prepare("INSERT INTO pedidos (user_id, total, estado, nombre_cliente, email_cliente, pais, ciudad, direccion, telefono, metodo_pago) VALUES (?, ?, 'Pending', ?, ?, ?, ?, ?, ?, ?)");
+        $stmt_order = $conn->prepare("INSERT INTO pedidos (user_id, total, estado, nombre_cliente, email_cliente, pais, ciudad, direccion, telefono, metodo_pago) VALUES (?, ?, 'Pendiente', ?, ?, ?, ?, ?, ?, ?)");
         $stmt_order->bind_param("idsssssss", $user_id, $total, $nombre, $email, $pais, $ciudad, $direccion, $telefono, $metodo);
         $stmt_order->execute();
         $pedido_id = $conn->insert_id;
