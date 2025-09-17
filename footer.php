6e30c4b5bcf3c07c4802f604f7da4c8d2e6f9933
diegoriya09 <juandiegoriveray920@gmail.com>
2025-09-17 08:50:58 -0500

    chat

--- footer.php
+++ footer.php
@@ -41,3 +41,24 @@
       </footer>
    </div>
 </footer>
+
+<?php include 'modal.php'; ?>
+<script src="/js/auth.js?v=<?php echo time(); ?>"></script>
+<script src="/js/index.js?v=<?php echo time(); ?>"></script>
+
+<script type="text/javascript">
+   var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+   (function() {
+      var s1 = document.createElement("script"),
+         s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/TU_PROPERTY_ID/TU_WIDGET_ID'; // <-- Aquí estará tu ID real
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+   })();
+</script>
+</body>
+
+</html>
